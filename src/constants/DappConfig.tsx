83e00e0be054621bcc9cd706529ffa29aec1d0b4
Suru <martianacademy@gmail.com>
2023-04-01 12:27:37 +0530

    clear the index.tsx for better visibility

---
+++ src/constants/DappConfig.tsx
@@ -0,0 +1,48 @@
+import { BSCTestnet, Config, Polygon } from "@usedapp/core";
+import { WalletConnectConnector } from "@usedapp/wallet-connect-connector";
+import { getDefaultProvider } from "ethers";
+
+export const DappConfig: Config = {
+  notifications: {
+    expirationPeriod: 5000,
+  },
+  readOnlyChainId: BSCTestnet?.chainId,
+  readOnlyUrls: {
+    [BSCTestnet?.chainId]: getDefaultProvider(
+      "https://data-seed-prebsc-1-s1.binance.org:8545/"
+    ),
+    [Polygon.chainId]: "https://polygon-rpc.com",
+  },
+  networks: [Polygon, BSCTestnet],
+  connectors: {
+    walletConnect: new WalletConnectConnector({
+      rpc: {
+        97: "https://data-seed-prebsc-1-s1.binance.org:8545/",
+        [Polygon.chainId]: "https://polygon-rpc.com",
+      },
+      qrcodeModalOptions: {
+        desktopLinks: [
+          "metamask",
+          "ledger",
+          "tokenary",
+          "wallet",
+          "wallet 3",
+          "secuX",
+          "ambire",
+          "wallet3",
+          "apolloX",
+          "zerion",
+          "sequence",
+          "punkWallet",
+          "kryptoGO",
+          "nft",
+          "riceWallet",
+          "vision",
+          "keyring",
+        ],
+        mobileLinks: ["metamask", "trust"],
+      },
+    }),
+  },
+  refresh: "everyBlock",
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,58 +1,11 @@
 import { ChakraProvider, ColorModeScript, theme } from "@chakra-ui/react";
-import { BSCTestnet, Config, DAppProvider, Polygon } from "@usedapp/core";
-import { WalletConnectConnector } from "@usedapp/wallet-connect-connector";
-import { getDefaultProvider } from "ethers";
+import { DAppProvider } from "@usedapp/core";
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import { DappConfig } from "./constants/DappConfig";
 import { router } from "./navigation/Routes";
-import reportWebVitals from "./reportWebVitals";
 import * as serviceWorker from "./serviceWorker";
-
-const dappConfig: Config = {
-  notifications: {
-    expirationPeriod: 5000,
-  },
-  readOnlyChainId: BSCTestnet?.chainId,
-  readOnlyUrls: {
-    [BSCTestnet?.chainId]: getDefaultProvider(
-      "https://data-seed-prebsc-1-s1.binance.org:8545/"
-    ),
-    [Polygon.chainId]: "https://polygon-rpc.com",
-  },
-  networks: [Polygon, BSCTestnet],
-  connectors: {
-    walletConnect: new WalletConnectConnector({
-      rpc: {
-        97: "https://data-seed-prebsc-1-s1.binance.org:8545/",
-        [Polygon.chainId]: "https://polygon-rpc.com",
-      },
-      qrcodeModalOptions: {
-        desktopLinks: [
-          "metamask",
-          "ledger",
-          "tokenary",
-          "wallet",
-          "wallet 3",
-          "secuX",
-          "ambire",
-          "wallet3",
-          "apolloX",
-          "zerion",
-          "sequence",
-          "punkWallet",
-          "kryptoGO",
-          "nft",
-          "riceWallet",
-          "vision",
-          "keyring",
-        ],
-        mobileLinks: ["metamask", "trust"],
-      },
-    }),
-  },
-  refresh: "everyBlock",
-};
 
 const container = document.getElementById("root");
 if (!container) throw new Error("Failed to find the root element");
@@ -60,7 +13,7 @@ const root = ReactDOM.createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <DAppProvider config={dappConfig}>
+    <DAppProvider config={DappConfig}>
       <ChakraProvider theme={theme}>
         <ColorModeScript />
         <RouterProvider router={router}></RouterProvider>
@@ -69,12 +22,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
 serviceWorker.unregister();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
